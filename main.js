38609b6ccfe643d6a26ed16b1580da57032209d1
Tanu <tanupandey291099@gmail.comgit config --global user.email tanupandey291099@gmail.com>
2023-12-11 15:50:11 +0530

    added more details

---
+++ main.js
@@ -0,0 +1,15 @@
+const submit=document.querySelector(".Submit_b");
+console.log(submit);
+
+const class1=document.querySelector(".rate-card");
+const class2=document.querySelector(".thank_you_state");
+submit.addEventListener('click', function(){
+    class1.classList.toggle("hidden");
+    class2.classList.toggle("hidden");
+    
+
+    
+
+
+
+});
