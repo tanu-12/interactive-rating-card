e3dc7e12c31eea429c0bbb35e6cd1b25d1ca18f0
Tanu <tanupandey291099@gmail.comgit config --global user.email tanupandey291099@gmail.com>
2023-12-13 21:41:22 +0530

    added more details

--- main.js
+++ main.js
@@ -3,13 +3,19 @@ console.log(submit);
 
 const class1=document.querySelector(".rate-card");
 const class2=document.querySelector(".thank_you_state");
+const rating=document.querySelectorAll(".rate");
+const f_rate=document.querySelector(".f_rate");
+for (let i=0;i<rating.length;i++){
+
+rating[i].addEventListener('click',function(e){
+    f_rate.textContent=e.target.value
+
+});
+
+}
+
+
 submit.addEventListener('click', function(){
     class1.classList.toggle("hidden");
     class2.classList.toggle("hidden");
-    
-
-    
-
-
-
-});
+    });
